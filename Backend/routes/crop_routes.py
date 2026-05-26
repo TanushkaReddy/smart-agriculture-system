2bbdecc3835b66b630e1c43bd02170b43d5f9cf0
Tanushka Reddy <tanushkareddy@gmail.com>
2026-05-26 14:29:23 +0530

    fixed issues

--- Backend/routes/crop_routes.py
+++ Backend/routes/crop_routes.py
@@ -4,7 +4,7 @@ from services.crop_service import predict_crop
 
 router = APIRouter()
 
-@router.post("/predict")
+@router.post("crop/predict")
 def crop_predict(data: CropInput):
     result = predict_crop(data)
     return {"recommended_crop": result}
