77811b4fc3364eab625f2638198f1f028d96dd59
Tanushka Reddy <tanushkareddy@gmail.com>
2026-05-26 10:17:46 +0530

    Initial commit

--- ML models/yield_prediction/save_model.py
+++ ML models/yield_prediction/save_model.py
@@ -1,6 +1,6 @@
 import joblib
 from model_training import model
 
-joblib.dump(model, "../saved_models/yield_model.pkl")
+joblib.dump(model, "../saved_models/yield_model.pkl",compress=3)
 
 print("Model Saved Successfully")
