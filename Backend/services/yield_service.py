52095d6bf4c6dac94cbf20072de4a9b43938411d
Tanushka Reddy <tanushkareddy@gmail.com>
2026-05-26 22:36:57 +0530

    yield_service

--- Backend/services/yield_service.py
+++ Backend/services/yield_service.py
@@ -1,20 +1,20 @@
+import pandas as pd
 from models_loader import yield_model
-import numpy as np
 
 def predict_yield(data):
 
-    input_data = np.array([[ 
-        data.crop,
-        data.crop_year,
-        data.season,
-        data.state,
-        data.area,
-        data.production,
-        data.rainfall,
-        data.pesticide,
-        data.fertilizer
-    ]])
+    input_df = pd.DataFrame([{
+        "Crop": data.crop,
+        "Crop_Year": data.crop_year,
+        "Season": data.season,
+        "State": data.state,
+        "Area": data.area,
+        "Production": data.production,
+        "Annual_Rainfall": data.rainfall,
+        "Pesticide": data.pesticide,
+        "Fertilizer": data.fertilizer
+    }])
 
-    prediction = yield_model.predict(input_data)
+    prediction = yield_model.predict(input_df)
 
     return prediction[0]
